eb504a5899dfd9b8dc5ffea263af400a06e128c7
David Engelmann <david+4113@satisfilabs.com>
2023-06-12 16:05:07 -0400

    add test for sample_request_with_body

--- test/test_request.ml
+++ test/test_request.ml
@@ -15,3 +15,31 @@ let sample_request_without_body : Request.request = {
     headers = [("User-Agent", "david-engelmann/bluesky (OCaml SDK)")];
     body = None
 }
+
+let test_sample_request_with_body_method_ _ =
+  match sample_request_with_body with
+   | { method_; _ } ->
+      OUnit2.assert_equal Http_method.Get method_
+
+let test_sample_request_with_body_url _ =
+  match sample_request_with_body with
+   | { url; _ } ->
+      OUnit2.assert_equal "https://github.com/david-engelmann" url
+
+let test_sample_request_with_body_headers _ =
+  match sample_request_with_body with
+   | { headers; _ } ->
+    match headers with
+     | (param_name, _) :: _ ->
+       OUnit2.assert_equal "User-Agent" param_name
+     | _ -> OUnit2.assert_equal 0 1
+
+let test_sample_request_with_body_body _ =
+  match sample_request_with_body with
+   | { body; _ } ->
+     match body with
+      | Some b ->
+        OUnit2.assert_equal "{\"July\": \"Jackson\"}" b
+      | None ->
+        OUnit2.assert_equal 0 1
+
