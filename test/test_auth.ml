5cba23425959191f160f25fc90d978dcdf252b6d
David Engelmann <david+4113@satisfilabs.com>
2023-06-12 19:15:59 -0400

    use ints for creation and expiry times

--- test/test_auth.ml
+++ test/test_auth.ml
@@ -2,16 +2,16 @@ open OUnit2
 open Bluesky.Auth
 
 let sample_auth_without_jti : Auth.auth = {
-    exp = "06-13-2023 22:29:34";
-    iat = "06-12-2023 22:29:34";
+    exp = 1686612561;
+    iat = 1686611561;
     scope = "read write delete";
     did = "123";
     jti = None;
   }
 
 let sample_auth_with_jti : Auth.auth = {
-    exp = "06-13-2023 22:29:34";
-    iat = "06-12-2023 22:29:34";
+    exp = 1686612561;
+    iat = 1686611561;
     scope = "read write delete";
     did = "321";
     jti = Some "jti";
@@ -20,12 +20,12 @@ let sample_auth_with_jti : Auth.auth = {
 let test_sample_auth_with_jti_exp _ =
     match sample_auth_with_jti with
      | { exp; _ } ->
-        OUnit2.assert_equal "06-13-2023 22:29:34" exp
+        OUnit2.assert_equal 1686612561 exp
 
 let test_sample_auth_with_jti_iat _ =
     match sample_auth_with_jti with
      | { iat; _ } ->
-        OUnit2.assert_equal "06-12-2023 22:29:34" iat
+        OUnit2.assert_equal 1686611561 iat
 
 let test_sample_auth_with_jti_scope _ =
     match sample_auth_with_jti with
@@ -49,12 +49,12 @@ let test_sample_auth_with_jti_jti _ =
 let test_sample_auth_without_jti_exp _ =
     match sample_auth_without_jti with
      | { exp; _ } ->
-        OUnit2.assert_equal "06-13-2023 22:29:34" exp
+        OUnit2.assert_equal 1686612561 exp
 
 let test_sample_auth_without_jti_iat _ =
     match sample_auth_without_jti with
      | { iat; _ } ->
-        OUnit2.assert_equal "06-12-2023 22:29:34" iat
+        OUnit2.assert_equal 1686611561 iat
 
 let test_sample_auth_without_jti_scope _ =
     match sample_auth_without_jti with
@@ -64,7 +64,7 @@ let test_sample_auth_without_jti_scope _ =
 let test_sample_auth_without_jti_did _ =
     match sample_auth_without_jti with
      | { did; _ } ->
-        OUnit2.assert_equal "321" did
+        OUnit2.assert_equal "123" did
 
 let test_sample_auth_without_jti_jti _ =
     match sample_auth_without_jti with
