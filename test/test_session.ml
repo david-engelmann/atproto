88242dcdae29f522f0aed81d067c6b1b06413dd7
David Engelmann <david+4113@satisfilabs.com>
2023-06-12 10:14:24 -0400

    add test for the session record type

---
+++ test/test_session.ml
@@ -0,0 +1,36 @@
+open OUnit2
+open Bluesky.Session
+
+let sample_session : Session =
+    {
+      username = "your.name";
+      password = "password";
+      atp_host = "https://bsky.social";
+      atp_auth_token = "JWToken"; (* set after login *)
+      did = "did_public_key"; (* set after login *)
+    }
+
+let test_sample_session_username _ =
+    match sample_session with
+     | { username; _ } ->
+        OUnit2.assert_equal "your.name" username
+
+let test_sample_session_password _ =
+    match sample_session with
+     | { password; _ } ->
+        OUnit2.assert_equal "password" password
+
+let test_sample_session_atp_host _ =
+    match sample_session with
+     | { atp_host; _ } ->
+        OUnit2.assert_equal "https://bsky.social" atp_host
+
+let test_sample_session_atp_auth_token _ =
+    match sample_session with
+     | { atp_auth_token; _ } ->
+        OUnit2.assert_equal "JWToken" atp_auth_token
+
+let test_sample_session_did _ =
+    match sample_session with
+     | { did; _ } ->
+        OUnit2.assert_equal "did_public_key" did
