41520c4fd4b0a46df9a4f95d07ad649b5075d0d4
David Engelmann <david+4113@satisfilabs.com>
2023-06-12 14:43:16 -0400

    write test for lookup_method

--- test/test_method.ml
+++ test/test_method.ml
@@ -3,3 +3,9 @@ open Bluesky.Method
 
 let sample_get_method : Method.method = Get
 let sample_post_method : Method.method = Post
+
+let test_lookup_method_with_get _ =
+  OUnit2.assert_equal (Method.lookup_method "get") sample_get_method
+
+let test_lookup_method_with_post _ =
+  OUnit2.assert_equal (Method.lookup_method "post") sample_post_method
