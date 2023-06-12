14282c954546af8eaf0eaa0e26e17a1287173c9e
David Engelmann <david+4113@satisfilabs.com>
2023-06-12 11:30:24 -0400

    add test for user record type

--- test/test_user.ml
+++ test/test_user.ml
@@ -6,3 +6,21 @@ let sample_user : User.user = {
       password = "password";
     }
 
+let test_sample_user_username _ =
+  match sample_user with
+   | { username; _ } ->
+      OUnit2.assert_equal "your.name" username
+
+let test_sample_user_password _ =
+  match sample_user with
+   | { password; _ } ->
+      OUnit2.assert_equal "password" password
+
+let suite =
+  "suite"
+  >::: [
+         "test_sample_user_username" >:: test_sample_user_username;
+         "test_sample_user_password" >:: test_sample_user_password;
+       ]
+
+let () = run_test_tt_main suite
