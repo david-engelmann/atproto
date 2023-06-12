67d9550b86f90748f3e0314806f5ad7b445bb136
David Engelmann <david+4113@satisfilabs.com>
2023-06-12 14:16:43 -0400

    get all response test to pass

--- test/test_response.ml
+++ test/test_response.ml
@@ -1,19 +1,19 @@
 open OUnit2
 open Bluesky.Response
 
-let sample_content : bytes = Bytes.create 12;
+let sample_content : bytes = Bytes.create 12;;
 Bytes.set sample_content 0 'J';
 Bytes.set sample_content 1 'u';
 Bytes.set sample_content 2 'l';
 Bytes.set sample_content 3 'y';
-Bytes.set sample_content 4 0x20;
+Bytes.set sample_content 4 (Char.chr 0x20);
 Bytes.set sample_content 5 'J';
 Bytes.set sample_content 6 'a';
 Bytes.set sample_content 7 'c';
 Bytes.set sample_content 8 'k';
 Bytes.set sample_content 9 's';
 Bytes.set sample_content 10 'o';
-Bytes.set sample_content 11 'n';
+Bytes.set sample_content 11 'n';;
 
 let sample_response : Response.response = {
     success = true;
@@ -35,7 +35,7 @@ let test_sample_response_status_code _ =
 let test_sample_response_content _ =
   match sample_response with
    | { content; _ } ->
-      OUnit2.assert_equal "July Jackson" Bytes.to_string content
+      OUnit2.assert_equal "July Jackson" (Bytes.to_string content)
 
 let test_sample_response_headers _ =
   match sample_response with
@@ -43,6 +43,7 @@ let test_sample_response_headers _ =
      match headers with
       | (param_name, _) :: _ ->
         OUnit2.assert_equal "User-Agent" param_name
+      | _ -> OUnit2.assert_equal 0 1
 
 let suite =
   "suite"
