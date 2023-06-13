bd87011096e77b8bd47b99c48e40032a77a28147
David Engelmann <david+4113@satisfilabs.com>
2023-06-12 20:58:45 -0400

    starting on login function

--- src/session.ml
+++ src/session.ml
@@ -7,4 +7,8 @@ module Session = struct
           atp_auth_token : string;
           did : string;
         }
+
+    let login (username : string) (password: string) : session =
+        let login_data (string, string) list = [("identifier", username); ("password", password)] in
+        
 end
