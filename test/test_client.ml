22b0f74bb56f7c1184489a41321b378f3df5d8b2
David Engelmann <david+4113@satisfilabs.com>
2023-06-13 10:20:00 -0400

    add a testing suite for the http client

--- test/test_client.ml
+++ test/test_client.ml
@@ -3,3 +3,12 @@ open Bluesky.Http_client
 
 let test_http_client_with_quotes_to_scrape _ =
     Http_client.start_client "https://quotes.toscrape.com"
+    OUnit2.assert_equal 1 1
+
+let suite =
+  "suite"
+  >::: [
+         "test_http_client_with_quotes_to_scrape" >:: test_http_client_with_quotes_to_scrape;
+       ]
+
+let () = run_test_tt_main suite
