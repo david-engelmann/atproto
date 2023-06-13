e708d1f20bfdbe3f19f06349ccd4444b338c7e77
David Engelmann <david+4113@satisfilabs.com>
2023-06-12 20:18:04 -0400

    add test for uri without search params

--- test/test_uri.ml
+++ test/test_uri.ml
@@ -42,3 +42,26 @@ let test_sample_uri_with_search_params_search_params _ =
        | None -> OUnit2.assert_equal 0 1
 
 
+let test_sample_uri_without_search_params_host _ =
+    match sample_uri_without_search_params with
+     | { host; _ } ->
+        OUnit2.assert_equal "david.host.com" host
+
+let test_sample_uri_without_search_params_path_name _ =
+    match sample_uri_without_search_params with
+     | { path_name; _ } ->
+        OUnit2.assert_equal "io.example.song" path_name
+
+let test_sample_uri_without_search_params_hash _ =
+    match sample_uri_without_search_params with
+     | { hash; _ } ->
+        OUnit2.assert_equal "3yI5-c1z-cc2p-1a" hash
+
+let test_sample_uri_without_search_params_search_params _ =
+    match sample_uri_without_search_params with
+     | { search_params; _ } ->
+      match search_params with
+       | None -> OUnit2.assert_equal 1 1
+       | Some _ -> OUnit2.assert_equal 0 1
+
+
