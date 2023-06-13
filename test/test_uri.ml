6f4511cc35187eec7dd2a485a67bdb7395710dd0
David Engelmann <david+4113@satisfilabs.com>
2023-06-12 20:16:24 -0400

    create test for uri with search params

--- test/test_uri.ml
+++ test/test_uri.ml
@@ -14,3 +14,31 @@ let sample_uri_with_search_params : Uri.uri = {
       hash = "3yI5-c1z-cc2p-1a";
       search_params = Some [("july", "jackson")];
     }
+
+let test_sample_uri_with_search_params_host _ =
+    match sample_uri_with_search_params with
+     | { host; _ } ->
+        OUnit2.assert_equal "david.host.com" host
+
+let test_sample_uri_with_search_params_path_name _ =
+    match sample_uri_with_search_params with
+     | { path_name; _ } ->
+        OUnit2.assert_equal "io.example.song" path_name
+
+let test_sample_uri_with_search_params_hash _ =
+    match sample_uri_with_search_params with
+     | { hash; _ } ->
+        OUnit2.assert_equal "3yI5-c1z-cc2p-1a" hash
+
+let test_sample_uri_with_search_params_search_params _ =
+    match sample_uri_with_search_params with
+     | { search_params; _ } ->
+      match search_params with
+       | Some p ->
+         match p with
+          | (param_name, _) :: _ ->
+            OUnit2.assert_equal "july" param_name
+          | _ -> OUnit2.assert_equal 0 1
+       | None -> OUnit2.assert_equal 0 1
+
+
