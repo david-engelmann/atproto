742e7384a11eb9f14d0101460a49ec3080932255
David Engelmann <david+4113@satisfilabs.com>
2023-06-13 20:27:22 -0400

    try to use just the first address

--- src/http_client.ml
+++ src/http_client.ml
@@ -63,25 +63,24 @@ module Http_client = struct
             addr >>= fun addr_info ->
             let socket = Lwt_unix.socket Unix.PF_INET Unix.SOCK_STREAM 0 in
             Lwt_unix.connect socket addr_info.Unix.ai_addr >>= fun () ->
+            let request =
+                Request.create
+                `GET
+                "/"
+                ~scheme:"https"
+                ~headers:
+                    Headers.(add_list empty [ ":authority", host ])
+            in
+            let response_received, notify_response_received = Lwt.wait () in
+            let response_handler = response_handler notify_response_received in
+            Client.TLS.create_connection_with_default ~error_handler socket
+            >>= fun connection ->
 
-          let request =
-              Request.create
-               `GET
-               "/"
-               ~scheme:"https"
-               ~headers:
-                   Headers.(add_list empty [ ":authority", host ])
-          in
-          let response_received, notify_response_received = Lwt.wait () in
-          let response_handler = response_handler notify_response_received in
-          Client.TLS.create_connection_with_default ~error_handler socket
-          >>= fun connection ->
-
-          let request_body =
-            Client.TLS.request connection request ~error_handler ~response_handler
-          in
-          Body.Writer.close request_body;
-          response_received )
+            let request_body =
+              Client.TLS.request connection request ~error_handler ~response_handler
+            in
+            Body.Writer.close request_body;
+            response_received) [List.hd lwt_list])
 
     (*
     let start_client (host : string) (port : int) =
