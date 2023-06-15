45d5f05b21efacc1f6d61f1e024640142dc13641
David Engelmann <david+4113@satisfilabs.com>
2023-06-15 09:41:43 -0400

    add httpbin request test

--- test/test_cohttp_client.ml
+++ test/test_cohttp_client.ml
@@ -1,15 +1,19 @@
 open OUnit2
 open Bluesky.Cohttp_client
 
-
-let test_http_client_with_quotes_to_scrape _ =
+let test_cohttp_client_with_quotes_to_scrape _ =
   let body = Lwt_main.run (Cohttp_client.get_host "quotes.toscrape.com" 443) in
-  assert_bool "Body is not empty" (body <> "")
+  OUnit2.assert_bool "Body is not empty" (body <> "")
+
+let test_cohttp_client_with_httpbin _ =
+  let body = Lwt_main.run (Cohttp_client.get_host "httpbin.org" 80) in
+  OUnit2.assert_bool "Body is not empty" (body <> "")
 
 let suite =
   "suite"
   >::: [
-        "test_http_client_with_quotes_to_scrape" >:: test_http_client_with_quotes_to_scrape;
+        "test_cohttp_client_with_quotes_to_scrape" >:: test_cohttp_client_with_quotes_to_scrape;
+        "test_cohttp_client_with_httpbin" >:: test_cohttp_client_with_httpbin;
        ]
 
 let () = run_test_tt_main suite
