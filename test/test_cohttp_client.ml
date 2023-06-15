977fb27fb1e6bfad009a181c816522d50c90ae83
David Engelmann <david+4113@satisfilabs.com>
2023-06-15 09:45:36 -0400

    quotes.toscrape on port 80 as well

--- test/test_cohttp_client.ml
+++ test/test_cohttp_client.ml
@@ -2,7 +2,7 @@ open OUnit2
 open Bluesky.Cohttp_client
 
 let test_cohttp_client_with_quotes_to_scrape _ =
-  let body = Lwt_main.run (Cohttp_client.get_host "quotes.toscrape.com" 443) in
+  let body = Lwt_main.run (Cohttp_client.get_host "quotes.toscrape.com" 80) in
   OUnit2.assert_bool "Body is not empty" (body <> "")
 
 let test_cohttp_client_with_httpbin _ =
