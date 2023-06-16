ba602b5fa114dbc5a913efb84661da6f5336711d
David Engelmann <david+4113@satisfilabs.com>
2023-06-16 10:24:26 -0400

    return string for now

--- src/auth.ml
+++ src/auth.ml
@@ -8,7 +8,7 @@ module Auth = struct
           did : string;
           jti : string option;
         }
-    let make_auth_token_request (username : string) (password : string) (personal_data_server : string) : auth =
+    let make_auth_token_request (username : string) (password : string) (personal_data_server : string) : string =
       let url = Printf.sprintf "http://%s/xrpc/com.atproto.server.createSession" personal_data_server in
       let data = Printf.sprintf "{\"identifier\": \"%s\", \"password\": \"%s\"}" username password in
       let body = Lwt_main.run (Cohttp_client.post_data url data) in
