6e978b628025fe6130b5a7c04e65e67e4274cece
David Engelmann <david+4113@satisfilabs.com>
2023-06-16 10:42:35 -0400

    need to use https not http

--- src/auth.ml
+++ src/auth.ml
@@ -9,7 +9,7 @@ module Auth = struct
           jti : string option;
         }
     let make_auth_token_request (username : string) (password : string) (personal_data_server : string) : string =
-      let url = Printf.sprintf "http://%s/xrpc/com.atproto.server.createSession" personal_data_server in
+      let url = Printf.sprintf "https://%s/xrpc/com.atproto.server.createSession" personal_data_server in
       let data = Printf.sprintf "{\"identifier\": \"%s\", \"password\": \"%s\"}" username password in
       let body = Lwt_main.run (Cohttp_client.post_data url data) in
       body;
