6b401c001a0a9189e2f7c7ad027f61c2b8370775
David Engelmann <david+4113@satisfilabs.com>
2023-06-17 13:53:25 -0400

    cleanup comments in parse_auth

--- src/auth.ml
+++ src/auth.ml
@@ -32,33 +32,6 @@ module Auth = struct
         in
         { exp; iat; scope; did; jti }
       | Error _ -> failwith "Invalid JWT token"
-    (*
-    let parse_auth json : auth =
-      let open Yojson.Basic.Util in
-      let jwt = json |> member "accessJwt" |> Jwt.to_string in
-      match Jwt.of_string jwt with
-      | Ok jwt ->
-        (* essentially *)
-        (*
-        let exp = Jwt.get_int_claim jwt "exp" in
-        let iat = Jwt.get_int_claim jwt "iat" in
-        let scope = Jwt.get_string_claim jwt "scope" in
-        let did = Jwt.get_string_claim jwt "sub" in
-        let jti = Jwt.get_string_claim jwt "refreshJwt" in
-        *)
-        (*let claims = Jwt.claims jwt in
-        let exp = claims |> member "exp" |> to_int in
-        let iat = claims |> member "iat" |> to_int in
-        let scope = claims |> member "scope" |> to_string in
-        let did = claims |> member "sub" |> to_string in
-        let jti =
-          try Some (json |> member "refreshJwt" |> to_string |> from_string |> member "jti" |> to_string)
-          with _ -> None
-        *)
-        in
-        { exp; iat; scope; did; jti }
-      | Error _ -> failwith "Invalid JWT token"
-    *)
 
     let make_auth_token_request (username : string) (password : string) (personal_data_server : string) : string =
       let url = Printf.sprintf "https://%s/xrpc/com.atproto.server.createSession" personal_data_server in
