e0bf1b111008c3f6cf670fc5c66064f7878a05e2
David Engelmann <david+4113@satisfilabs.com>
2023-06-17 20:34:54 -0400

    use auth as a field in session

--- src/session.ml
+++ src/session.ml
@@ -6,24 +6,21 @@ module Session = struct
         username : string;
         password : string;
         atp_host : string;
-        atp_auth_token : string;
-        did : string;
+        auth : Auth.auth;
       }
 
   let create_session (username : string) (password: string) : session =
     let atp_host = "bsky.social" in
     let body = Auth.make_auth_token_request username password atp_host in
     let session_auth = body |> Auth.convert_body_to_json |> Auth.parse_auth in
-    let atp_auth_token = session_auth.token in
-    let did = session_auth.did in
-    { username; password; atp_host; atp_auth_token; did }
+    { username; password; atp_host; auth=session_auth }
 
   let bearer_token_from_session (s : session) : (string * string) =
       let bearer_header = "Bearer " ^ s.atp_auth_token in
       ("Authorization", bearer_header)
 
   let refresh_session_auth (s : session) : session =
-    if Auth.is_token_expired s. then
+    if Auth.is_token_expired s.auth then
       create_session s.username s.password
     else
       s
--- test/test_session.ml
+++ test/test_session.ml
@@ -5,8 +5,14 @@ let sample_session : Session.session = {
       username = "your.name";
       password = "password";
       atp_host = "https://bsky.social";
-      atp_auth_token = "JWToken"; (* set after login *)
-      did = "did_public_key"; (* set after login *)
+      auth = {
+        exp = 0;
+        iat = 0;
+        scope = "";
+        did = "did_public_key";
+        jti = None;
+        token = "JWToken";
+      };
     }
 
 let test_sample_session_username _ =
@@ -24,14 +30,14 @@ let test_sample_session_atp_host _ =
      | { atp_host; _ } ->
         OUnit2.assert_equal "https://bsky.social" atp_host
 
-let test_sample_session_atp_auth_token _ =
+let test_sample_session_auth_token _ =
     match sample_session with
-     | { atp_auth_token; _ } ->
-        OUnit2.assert_equal "JWToken" atp_auth_token
+     | { auth = { token; _ }; _ } ->
+        OUnit2.assert_equal "JWToken" token
 
-let test_sample_session_did _ =
+let test_sample_session_auth_did _ =
     match sample_session with
-     | { did; _ } ->
+     | { auth = { did; _ }; _ } ->
         OUnit2.assert_equal "did_public_key" did
 
 let test_create_session _ =
@@ -39,8 +45,8 @@ let test_create_session _ =
     OUnit2.assert_equal ~printer:string_of_bool true ((String.length test_session.username) > 0);
     OUnit2.assert_equal ~printer:string_of_bool true ((String.length test_session.password) > 0);
     OUnit2.assert_equal ~printer:string_of_bool true ((String.length test_session.atp_host) > 0);
-    OUnit2.assert_equal ~printer:string_of_bool true ((String.length test_session.atp_auth_token) > 0);
-    OUnit2.assert_equal ~printer:string_of_bool true ((String.length test_session.did) > 0)
+    OUnit2.assert_equal ~printer:string_of_bool true ((String.length test_session.auth.token) > 0);
+    OUnit2.assert_equal ~printer:string_of_bool true ((String.length test_session.auth.did) > 0)
 
 let test_bearer_token_from_session _ =
     let test_session = Session.create_session "david.engelmann44@gmail.com" "lsnv-tc3a-7wrl-upct" in
@@ -56,8 +62,8 @@ let suite =
          "test_sample_session_username" >:: test_sample_session_username;
          "test_sample_session_password" >:: test_sample_session_password;
          "test_sample_session_atp_host" >:: test_sample_session_atp_host;
-         "test_sample_session_atp_auth_token" >:: test_sample_session_atp_auth_token;
-         "test_sample_session_did" >:: test_sample_session_did;
+         "test_sample_session_auth_token" >:: test_sample_session_auth_token;
+         "test_sample_session_auth_did" >:: test_sample_session_auth_did;
          "test_create_session" >:: test_create_session;
          "test_bearer_token_from_session" >:: test_bearer_token_from_session;
        ]
