08c51f82cbe19bc35cd743115292a3121e3dffc3
David Engelmann <david+4113@satisfilabs.com>
2023-06-19 18:35:51 -0400

    add test for get_profile

--- src/app.ml
+++ src/app.ml
@@ -9,12 +9,10 @@ module App = struct
 
   let get_profile (s : session) (actor : string) =
     let base_endpoint = base_endpoint_from_env in
-    let (username, password) = Auth.username_and_password_from_env in
-    let sesh = Session.create_session username password in
-    let bearer_token = Session.bearer_token_from_session sesh in
+    let bearer_token = Session.bearer_token_from_session s in
     let application_json = Cohttp_client.application_json_setting_tuple in
     let headers = Cohttp_client.create_headers_from_pairs [application_json; bearer_token] in
-    let get_profile_url = sesh.atp_host ^ "/" ^ base_endpoint ^ "app.bsky.actor.getProfile" in
+    let get_profile_url = s.atp_host ^ "/" ^ base_endpoint ^ "app.bsky.actor.getProfile" in
     let body = Cohttp_client.create_body_from_pairs [("actor", actor)] in
     let results = Lwt_main.run (Cohttp_client.get_request_with_body_and_headers get_profile_url body headers) in
     results
--- test/test_app.ml
+++ test/test_app.ml
@@ -3,4 +3,9 @@ open Bluesky.Session
 open Bluesky.Auth
 open Bluesky.App
 
-
+let test_get_profile _ =
+  let (username, password) = Auth.username_and_password_from_env in
+  let test_session = Session.create_session username password in
+  let profile = App.get_profile test_session "david-engelmann.bsky.social" in
+  Printf.printf "Profile: %d\n" profile;
+  OUnit2.assert_bool "Body is not empty" (body <> "")
