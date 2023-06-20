8ef682572855cf17e765093b61e2d611b87cbaeb
David Engelmann <david+4113@satisfilabs.com>
2023-06-19 20:20:29 -0400

    add test for get_profiles

--- test/test_app.ml
+++ test/test_app.ml
@@ -10,11 +10,19 @@ let test_get_profile _ =
   Printf.printf "Profile: %s\n" profile;
   OUnit2.assert_bool "Profile is not empty" (profile <> "")
 
+let test_get_profiles _ =
+  let (username, password) = Auth.username_and_password_from_env in
+  let test_session = Session.create_session username password in
+  let profiles = App.get_profiles test_session ["david-engelmann.bsky.social"; "jay.bsky.team"] in
+  Printf.printf "Profiles: %s\n" profiles;
+  OUnit2.assert_bool "Profiles is not empty" (profiles <> "")
+
 
 let suite =
     "suite"
     >::: [
            "test_get_profile" >:: test_get_profile;
+           "test_get_profiles" >:: test_get_profiles;
          ]
 
 let () = run_test_tt_main suite
