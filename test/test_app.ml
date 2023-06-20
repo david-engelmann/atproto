e43be2e9ddc7e3879d0e29cff4587892bf4a8f5b
David Engelmann <david+4113@satisfilabs.com>
2023-06-19 23:18:26 -0400

    add test for get_suggestions

--- test/test_app.ml
+++ test/test_app.ml
@@ -18,11 +18,21 @@ let test_get_profiles _ =
   OUnit2.assert_bool "Profiles is not empty" (profiles <> "")
 
 
+let test_get_suggestions _ =
+  let (username, password) = Auth.username_and_password_from_env in
+  let test_session = Session.create_session username password in
+  let suggestions = App.get_suggestions test_session 5 in
+  Printf.printf "Suggestions: %s\n" suggestions;
+  OUnit2.assert_bool "Suggestions is not empty" (suggestions <> "")
+
+
+
 let suite =
     "suite"
     >::: [
            "test_get_profile" >:: test_get_profile;
            "test_get_profiles" >:: test_get_profiles;
+           "test_get_suggestions" >:: test_get_suggestions;
          ]
 
 let () = run_test_tt_main suite
