3af8a4ffc7dca37cc214ccd10ca01f822f75884c
David Engelmann <david+4113@satisfilabs.com>
2023-06-20 14:24:21 -0400

    use create_test_session in test_actor

--- test/test_actor.ml
+++ test/test_actor.ml
@@ -3,37 +3,36 @@ open Atproto.Session
 open Atproto.Auth
 open Atproto.Actor
 
+let create_test_session _ =
+    let (username, password) = Auth.username_and_password_from_env in
+    Session.create_session username password
+
 let test_get_profile _ =
-  let (username, password) = Auth.username_and_password_from_env in
-  let test_session = Session.create_session username password in
+  let test_session = create_test_session () |> Session.refresh_session_auth in
   let profile = Actor.get_profile test_session "david-engelmann.bsky.social" in
   Printf.printf "Profile: %s\n" profile;
   OUnit2.assert_bool "Profile is not empty" (profile <> "")
 
 let test_get_profiles _ =
-  let (username, password) = Auth.username_and_password_from_env in
-  let test_session = Session.create_session username password in
+  let test_session = create_test_session () |> Session.refresh_session_auth in
   let profiles = Actor.get_profiles test_session ["david-engelmann.bsky.social"; "jay.bsky.team"] in
   Printf.printf "Profiles: %s\n" profiles;
   OUnit2.assert_bool "Profiles is not empty" (profiles <> "")
 
 let test_get_suggestions _ =
-  let (username, password) = Auth.username_and_password_from_env in
-  let test_session = Session.create_session username password in
+  let test_session = create_test_session () |> Session.refresh_session_auth in
   let suggestions = Actor.get_suggestions test_session 5 in
   Printf.printf "Suggestions: %s\n" suggestions;
   OUnit2.assert_bool "Suggestions is not empty" (suggestions <> "")
 
 let test_search_actors _ =
-  let (username, password) = Auth.username_and_password_from_env in
-  let test_session = Session.create_session username password in
+  let test_session = create_test_session () |> Session.refresh_session_auth in
   let profiles = Actor.search_actors test_session "david-engelmann" 1 in
   Printf.printf "Search Profiles: %s\n" profiles;
   OUnit2.assert_bool "Search Profiles is not empty" (profiles <> "")
 
 let test_search_actors_typeahead _ =
-  let (username, password) = Auth.username_and_password_from_env in
-  let test_session = Session.create_session username password in
+  let test_session = create_test_session () |> Session.refresh_session_auth in
   let profiles = Actor.search_actors_typeahead test_session "david-engelmann" 1 in
   Printf.printf "Search Profiles Typeahead: %s\n" profiles;
   OUnit2.assert_bool "Search Profiles Typeahead is not empty" (profiles <> "")
