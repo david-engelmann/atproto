a17a733316b2a4095a3bfa2c6edf84a2b1ff0aff
David Engelmann <david+4113@satisfilabs.com>
2023-06-21 15:28:38 -0400

    add file for testing sync endpoints

---
+++ test/test_sync.ml
@@ -0,0 +1,16 @@
+open OUnit2
+open Atproto.Session
+open Atproto.Auth
+open Atproto.Sync
+
+let create_test_session _ =
+    let (username, password) = Auth.username_and_password_from_env in
+    Session.create_session username password
+
+
+let suite =
+    "suite"
+    >::: [
+         ]
+
+let () = run_test_tt_main suite
