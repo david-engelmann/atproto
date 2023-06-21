3ccd0335aec11c8e65e697650f9544ab6c626a8e
David Engelmann <david+4113@satisfilabs.com>
2023-06-21 12:45:46 -0400

    fix double slash issue

--- src/cohttp_client.ml
+++ src/cohttp_client.ml
@@ -83,7 +83,7 @@ module Cohttp_client = struct
     |> fun h -> Header.add h "Content-Type" "application/json" in
     let body = Cohttp_lwt.Body.of_string data in
     Client.post ~headers ~body (Uri.of_string url) >>= fun (resp, body) ->
-    let code = resp |> Response.status |> Code.code_of_status in
+    let _ = resp |> Response.status |> Code.code_of_status in
     (*Printf.printf "Response Code: %d\n" code;*)
     (*Printf.printf "Headers: %s\n" (resp |> Response.headers |> Header.to_string);*)
     body |> Cohttp_lwt.Body.to_string >|= fun body ->
@@ -94,7 +94,7 @@ module Cohttp_client = struct
     let open Lwt.Infix in
     let url_with_body = url ^ "?" ^ body in
     Client.get ~headers (Uri.of_string url_with_body) >>= fun (resp, body) ->
-    let code = resp |> Response.status |> Code.code_of_status in
+    let _ = resp |> Response.status |> Code.code_of_status in
     (*Printf.printf "Response Code: %d\n" code;*)
     (*Printf.printf "Headers: %s\n" (resp |> Response.headers |> Header.to_string);*)
     body |> Cohttp_lwt.Body.to_string >|= fun body ->
@@ -104,7 +104,7 @@ module Cohttp_client = struct
   let get_request_with_headers (url : string) headers =
     let open Lwt.Infix in
     Client.get ~headers (Uri.of_string url) >>= fun (resp, body) ->
-    let code = resp |> Response.status |> Code.code_of_status in
+    let _ = resp |> Response.status |> Code.code_of_status in
     (*Printf.printf "Response Code: %d\n" code;*)
     (*Printf.printf "Headers: %s\n" (resp |> Response.headers |> Header.to_string);*)
     body |> Cohttp_lwt.Body.to_string >|= fun body ->
