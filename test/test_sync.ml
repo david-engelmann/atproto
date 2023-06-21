7fe07035d49e68453723b5ce821dcca057059a66
David Engelmann <david+4113@satisfilabs.com>
2023-06-21 15:54:13 -0400

    add test for get_blob

--- test/test_sync.ml
+++ test/test_sync.ml
@@ -7,10 +7,17 @@ let create_test_session _ =
     let (username, password) = Auth.username_and_password_from_env in
     Session.create_session username password
 
+let test_get_blob _ =
+  let test_session = create_test_session () |> Session.refresh_session_auth in
+  let blob = Sync.get_blob test_session "at://did:plc:xov3uvxfd4to6ev3ak5g5uxk/app.bsky.feed.post/3jyf6gx25eb27" "bafyreiarimgpoqvxxnf3sg4h52gvfzvmyeybxk2xgy6v3dra7zuldy73aq" in
+  Printf.printf "Blob: %s\n" blob;
+  OUnit2.assert_bool "Blob is not empty" (blob <> "")
+
 
 let suite =
     "suite"
     >::: [
+          "test_get_blob" >:: test_get_blob;
          ]
 
 let () = run_test_tt_main suite
