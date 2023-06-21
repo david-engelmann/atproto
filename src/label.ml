08325c29313691165732b2d8c0a490c746e11836
David Engelmann <david+4113@satisfilabs.com>
2023-06-21 14:40:18 -0400

    start on the label module

---
+++ src/label.ml
@@ -0,0 +1,18 @@
+open Session
+open Cohttp_client
+open App
+
+module Label = struct
+  let create_label_endpoint (query_name : string) : string =
+    "com.atproto.label" ^ "." ^ query_name
+
+  let query_labels (s : Session.session) (uri_patterns : string list) : string =
+    let bearer_token = Session.bearer_token_from_session s in
+    let application_json = Cohttp_client.application_json_setting_tuple in
+    let headers = Cohttp_client.create_headers_from_pairs [application_json; bearer_token] in
+    let base_url = App.create_base_url s in
+    let query_labels_url = App.create_endpoint_url base_url (create_label_endpoint "queryLabels") in
+    let body = Cohttp_client.add_query_params "uriPatterns" uri_patterns in
+    let labels = Lwt_main.run (Cohttp_client.get_request_with_body_and_headers query_labels_url body headers) in
+    labels
+end
