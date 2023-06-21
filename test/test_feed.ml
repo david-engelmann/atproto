c6d691eb121050b30e9682732c00ef578c6b817e
David Engelmann <david+4113@satisfilabs.com>
2023-06-21 09:45:05 -0400

    function for testing get_post_thread

--- test/test_feed.ml
+++ test/test_feed.ml
@@ -19,6 +19,13 @@ let test_get_likes _ =
   Printf.printf "Likes Feed: %s\n" likes;
   OUnit2.assert_bool "Likes Feed is not empty" (likes <> "")
 
+let test_get_post_thread _ =
+  let test_session = create_test_session () |> Session.refresh_session_auth in
+  let post_thread = Feed.get_post_thread test_session "at://did:plc:xov3uvxfd4to6ev3ak5g5uxk/app.bsky.feed.post/3jyf6gx25eb27" 1 in
+  Printf.printf "Post Thread Feed: %s\n" post_thread;
+  OUnit2.assert_bool "Post Thread Feed is not empty" (post_thread <> "")
+
+
 
 let suite =
     "suite"
