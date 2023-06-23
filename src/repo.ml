6810e6ea79e257761e75be1b28d57f4ae261c786
David Engelmann <david+4113@satisfilabs.com>
2023-06-23 09:58:55 -0400

    add the repo.ml fie

---
+++ src/repo.ml
@@ -0,0 +1,29 @@
+open Session
+open Cohttp_client
+open App
+
+module Repo = struct
+  let create_repo_endpoint (query_name : string) : string =
+    "com.atproto.repo" ^ "." ^ query_name
+
+  let describe_repo (s : Session.session) (repo : string) : string =
+    let bearer_token = Session.bearer_token_from_session s in
+    let application_json = Cohttp_client.application_json_setting_tuple in
+    let headers = Cohttp_client.create_headers_from_pairs [application_json; bearer_token] in
+    let base_url = App.create_base_url s in
+    let describe_repo_url = App.create_endpoint_url base_url (create_repo_endpoint "describeRepo") in
+    let body = Cohttp_client.create_body_from_pairs [("repo", repo)] in
+    let repo_description = Lwt_main.run (Cohttp_client.get_request_with_body_and_headers describe_repo_url body headers) in
+    repo_description
+
+  let get_record (s : Session.session) (repo : string) (collection : string) (rkey : string) (cid : string) : string =
+    let bearer_token = Session.bearer_token_from_session s in
+    let application_json = Cohttp_client.application_json_setting_tuple in
+    let headers = Cohttp_client.create_headers_from_pairs [application_json; bearer_token] in
+    let base_url = App.create_base_url s in
+    let get_record_url = App.create_endpoint_url base_url (create_repo_endpoint "getRecord") in
+    let body = Cohttp_client.create_body_from_pairs [("repo", repo); ("collection", collection); ("rkey", rkey); ("cid", cid)] in
+    let record = Lwt_main.run (Cohttp_client.get_request_with_body_and_headers get_record_url body headers) in
+    record
+
+end
