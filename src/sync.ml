65314ee64356e603a3a007a9738817d125f1b63f
David Engelmann <david+4113@satisfilabs.com>
2023-07-12 15:10:42 -0400

    try to simplify download for testing

--- src/sync.ml
+++ src/sync.ml
@@ -31,7 +31,6 @@ module Sync = struct
       let blob = Lwt_main.run (Cohttp_client.get_bytes_request_with_body_and_headers get_blob_url body headers) in
       Lwt_main.run (Lwt_io.with_file ~mode:Lwt_io.Output filename (fun oc -> Lwt_io.write oc blob))
     | _ -> ()
-  *)
   let download_image (s : Session.session) (did : string) (cid : string) (filename : string) : unit Lwt.t =
     let open Lwt.Infix in
     let bearer_token = Session.bearer_token_from_session s in
@@ -45,6 +44,20 @@ module Sync = struct
     | ct when List.mem ct ["image/jpeg"; "image/png"; "image/gif"; "image/bmp"; "image/webp"; "image/svg+xml"; "image/tiff"] ->
       Cohttp_client.get_bytes_request_with_body_and_headers get_blob_url body headers >>= fun blob ->
       Lwt_io.with_file ~mode:Lwt_io.Output filename (fun oc -> Lwt_io.write oc blob)
+    | _ -> Lwt.return ()
+  *)
+  let download_image (s : Session.session) (did : string) (cid : string) (filename : string) : unit Lwt.t =
+    let open Lwt.Infix in
+    let bearer_token = Session.bearer_token_from_session s in
+    let application_json = Cohttp_client.application_json_setting_tuple in
+    let headers = Cohttp_client.create_headers_from_pairs [application_json; bearer_token] in
+    let base_url = App.create_base_url s in
+    let get_blob_url = App.create_endpoint_url base_url (create_sync_endpoint "getBlob") in
+    let body = Cohttp_client.create_body_from_pairs [("did", did); ("cid", cid)] in
+    Cohttp_client.get_content_type_with_body_headers get_blob_url body headers >>= fun content_type ->
+    match content_type with
+    | ct when List.mem ct ["image/jpeg"; "image/png"; "image/gif"; "image/bmp"; "image/webp"; "image/svg+xml"; "image/tiff"] ->
+      Lwt_io.with_file ~mode:Lwt_io.Output filename (fun oc -> Lwt_io.write oc (get_blob s did cid))
     | _ -> Lwt.return ()
 
 end
--- test/test_sync.ml
+++ test/test_sync.ml
@@ -14,7 +14,7 @@ let test_get_blob _ =
 
 let test_download_image _ =
   let test_session = create_test_session () |> Session.refresh_session_auth in
-  let _ = Lwt_main.run (Sync.download_image test_session "did:plc:xov3uvxfd4to6ev3ak5g5uxk" "bafkreieva64qpnxs7zmwc6ezo7hatq4d22ot7wqlj4hi24zimjqzoye4wq" "test_image.jpeg") in
+  let _ = Sync.download_image test_session "did:plc:xov3uvxfd4to6ev3ak5g5uxk" "bafkreieva64qpnxs7zmwc6ezo7hatq4d22ot7wqlj4hi24zimjqzoye4wq" "test_image.jpeg" in
   OUnit2.assert_bool "File doesn't exist" (Sys.file_exists "test_image.jpeg")
 
 
@@ -22,7 +22,7 @@ let suite =
     "suite"
     >::: [
           "test_get_blob" >:: test_get_blob;
-         (*"test_download_image" >:: test_download_image;*)
+         "test_download_image" >:: test_download_image;
          ]
 
 let () = run_test_tt_main suite
