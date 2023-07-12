1c6e7e388d1b301fce79fa0cfdcd76a65e813adb
David Engelmann <david+4113@satisfilabs.com>
2023-07-12 10:21:27 -0400

    return a unit promise with download_image

--- src/sync.ml
+++ src/sync.ml
@@ -16,6 +16,7 @@ module Sync = struct
     let blob = Lwt_main.run (Cohttp_client.get_request_with_body_and_headers get_blob_url body headers) in
     blob
 
+  (*
   let download_image (s : Session.session) (did : string) (cid : string) (filename : string) : unit =
     let bearer_token = Session.bearer_token_from_session s in
     let application_json = Cohttp_client.application_json_setting_tuple in
@@ -30,5 +31,21 @@ module Sync = struct
       let blob = Lwt_main.run (Cohttp_client.get_bytes_request_with_body_and_headers get_blob_url body headers) in
       Lwt_main.run (Lwt_io.with_file ~mode:Lwt_io.Output filename (fun oc -> Lwt_io.write oc blob))
     | _ -> ()
+  *)
+  let download_image (s : Session.session) (did : string) (cid : string) (filename : string) : unit Lwt.t =
+    let open Lwt.Infix in
+    let bearer_token = Session.bearer_token_from_session s in
+    let application_json = Cohttp_client.application_json_setting_tuple in
+    let headers = Cohttp_client.create_headers_from_pairs [application_json; bearer_token] in
+    let base_url = App.create_base_url s in
+    let get_blob_url = App.create_endpoint_url base_url (create_sync_endpoint "getBlob") in
+    let body = Cohttp_client.create_body_from_pairs [("did", did); ("cid", cid)] in
+    Cohttp_client.get_content_type_with_body_headers get_blob_url body headers >>= fun content_type ->
+    Printf.printf "Content-Type: %s\n" content_type;
+    match content_type with
+    | ct when List.mem ct ["image/jpeg"; "image/png"; "image/gif"; "image/bmp"; "image/webp"; "image/svg+xml"; "image/tiff"] ->
+      Cohttp_client.get_bytes_request_with_body_and_headers get_blob_url body headers >>= fun blob ->
+      Lwt_io.with_file ~mode:Lwt_io.Output filename (fun oc -> Lwt_io.write oc blob)
+    | _ -> Lwt.return ()
 
 end
