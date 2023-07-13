ffddc6ccfbbac60c90d5a0982084195e3213e06e
David Engelmann <david+4113@satisfilabs.com>
2023-07-12 22:50:15 -0400

    use ? with type no option declared

--- src/server.ml
+++ src/server.ml
@@ -21,21 +21,25 @@ module Server = struct
     let headers = Cohttp_client.create_headers_from_pairs [application_json; bearer_token] in
     let base_url = App.create_base_url s in
     let create_account_url = App.create_endpoint_url base_url (create_server_endpoint "createAccount") in
-    match invite_code with
-    | Some actual_invite_code ->
-        match recovery_key with
-        | Some actual_recovery_key ->
-            let data = Printf.sprintf "{\"email\": \"%s\", \"handle\": \"%s\", \"password\": \"%s\", \"inviteCode\": \"%s\", \"recoveryKey\": \"%s\"}" email handle password actual_invite_code actual_recovery_key in
-        | None ->
-            let data = Printf.sprintf "{\"email\": \"%s\", \"handle\": \"%s\", \"password\": \"%s\", \"inviteCode\": \"%s\"}" email handle password actual_invite_code in
-    | None ->
-        match recovery_key with
-        | Some actual_recovery_key ->
-            let data = Printf.sprintf "{\"email\": \"%s\", \"handle\": \"%s\", \"password\": \"%s\", \"recoveryKey\": \"%s\"}" email handle password actual_invite_code actual_recovery_key in
-        | None ->
-            let data = Printf.sprintf "{\"email\": \"%s\", \"handle\": \"%s\", \"password\": \"%s\"}" email handle password actual_invite_code in
-    let created_account = Lwt_main.run (Cohttp_client.post_data_with_headers data create_account_url) in
+    let data =
+      match invite_code, recovery_key with
+      | Some actual_invite_code, Some actual_recovery_key -> Printf.sprintf "{\"email\": \"%s\", \"handle\": \"%s\", \"password\": \"%s\", \"inviteCode\": \"%s\", \"recoveryKey\": \"%s\"}" email handle password actual_invite_code actual_recovery_key
+      | Some actual_invite_code, None -> Printf.sprintf "{\"email\": \"%s\", \"handle\": \"%s\", \"password\": \"%s\", \"inviteCode\": \"%s\"}" email handle password actual_invite_code
+      | None, Some actual_recovery_key -> Printf.sprintf "{\"email\": \"%s\", \"handle\": \"%s\", \"password\": \"%s\", \"recoveryKey\": \"%s\"}" email handle password actual_recovery_key
+      | None, None -> Printf.sprintf "{\"email\": \"%s\", \"handle\": \"%s\", \"password\": \"%s\"}" email handle password
+    in
+    let created_account = Lwt_main.run (Cohttp_client.post_data_with_headers data create_account_url headers) in
     created_account
+
+  let create_app_password (s : Session.session) (name : string) : string =
+    let bearer_token = Session.bearer_token_from_session s in
+    let application_json = Cohttp_client.application_json_setting_tuple in
+    let headers = Cohttp_client.create_headers_from_pairs [application_json; bearer_token] in
+    let base_url = App.create_base_url s in
+    let create_app_password_url = App.create_endpoint_url base_url (create_server_endpoint "createAppPassword") in
+    let data = Printf.sprintf "{\"name\": \"%s\"}" name in
+    let created_app_password = Lwt_main.run (Cohttp_client.post_data_with_headers data create_app_password_url headers) in
+    created_app_password
 
 end
 
