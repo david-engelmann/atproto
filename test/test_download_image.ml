cf2dc52b8c2ffe678a0d7f945a9df36e05967120
David Engelmann <david+4113@satisfilabs.com>
2023-07-12 21:30:04 -0400

    split sync test into two files

---
+++ test/test_download_image.ml
@@ -0,0 +1,22 @@
+open OUnit2
+open Atproto.Session
+open Atproto.Auth
+open Atproto.Sync
+
+let create_test_session _ =
+    let (username, password) = Auth.username_and_password_from_env in
+    Session.create_session username password
+
+let test_download_image _ =
+  let test_session = create_test_session () |> Session.refresh_session_auth in
+  let _ = Lwt_main.run (Sync.download_image test_session "did:plc:xov3uvxfd4to6ev3ak5g5uxk" "bafkreieva64qpnxs7zmwc6ezo7hatq4d22ot7wqlj4hi24zimjqzoye4wq" "test_image.jpeg") in
+  OUnit2.assert_bool "File doesn't exist" (Sys.file_exists "test_image.jpeg")
+
+
+let suite =
+    "suite"
+    >::: [
+         "test_download_image" >:: test_download_image;
+         ]
+
+let () = run_test_tt_main suite
