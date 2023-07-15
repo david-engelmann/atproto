2cebddc4165babf4210dceeb230f0b08948275dc
David Engelmann <david+4113@satisfilabs.com>
2023-07-15 09:29:25 -0400

    add test for get_blocks

---
+++ test/test_blocks.ml
@@ -0,0 +1,22 @@
+open OUnit2
+open Atproto.Session
+open Atproto.Auth
+open Atproto.Sync
+
+let create_test_session _ =
+    let (username, password) = Auth.username_and_password_from_env in
+    Session.create_session username password
+
+let test_get_block _ =
+  let test_session = create_test_session () |> Session.refresh_session_auth in
+  let block = Lwt_main.run (Sync.get_block test_session "did:plc:xov3uvxfd4to6ev3ak5g5uxk" ["bafkreieva64qpnxs7zmwc6ezo7hatq4d22ot7wqlj4hi24zimjqzoye4wq"]) in
+  OUnit2.assert_bool "Block is not empty" (block <> "")
+
+
+let suite =
+    "suite"
+    >::: [
+         "test_get_block" >:: test_get_block;
+         ]
+
+let () = run_test_tt_main suite
