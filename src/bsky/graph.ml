f721d3b6eb55c41d2ae827734c2358f3001bafdc
david-engelmann <david.engelmann44@gmail.com>
2023-07-15 16:03:34 -0400

    add function for getFollowers

--- src/bsky/graph.ml
+++ src/bsky/graph.ml
@@ -15,4 +15,14 @@ module Graph = struct
     let body = Cohttp_client.create_body_from_pairs [("limit", string_of_int limit)] in
     let blocks = Lwt_main.run (Cohttp_client.get_request_with_body_and_headers get_blocks_url body headers) in
     blocks
+
+  let get_followers (s : Session.session) (actor : string) (limit : int) : string =
+    let bearer_token = Session.bearer_token_from_session s in
+    let application_json = Cohttp_client.application_json_setting_tuple in
+    let headers = Cohttp_client.create_headers_from_pairs [application_json; bearer_token] in
+    let base_url = App.create_base_url s in
+    let get_followers_url = App.create_endpoint_url base_url (create_sync_endpoint "getFollowers") in
+    let body = Cohttp_client.create_body_from_pairs [("actor", actor); ("limit", string_of_int limit)] in
+    let followers = Lwt_main.run (Cohttp_client.get_request_with_body_and_headers get_followers_url body headers) in
+    followers
 end
