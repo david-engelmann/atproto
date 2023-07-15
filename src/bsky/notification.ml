ec2554c8960d0bf44c10fb901bfe1d445abe371d
david-engelmann <david.engelmann44@gmail.com>
2023-07-15 18:00:26 -0400

    add function for updateSeen

--- src/bsky/notification.ml
+++ src/bsky/notification.ml
@@ -25,4 +25,14 @@ module Notification = struct
     let body = Cohttp_client.create_body_from_pairs [("limit", string_of_int limit); ("seenAt", seen_at)] in
     let notifications = Lwt_main.run (Cohttp_client.get_request_with_body_and_headers list_notifications_url body headers) in
     notifications
+
+  let update_seen (s : Session.session) (seen_at : string) : string =
+    let bearer_token = Session.bearer_token_from_session s in
+    let application_json = Cohttp_client.application_json_setting_tuple in
+    let headers = Cohttp_client.create_headers_from_pairs [application_json; bearer_token] in
+    let base_url = App.create_base_url s in
+    let get_update_seen_url = App.create_endpoint_url base_url (create_notification_endpoint "updateSeen") in
+    let body = Cohttp_client.create_body_from_pairs [("seenAt", seen_at)] in
+    let updated_seen = Lwt_main.run (Cohttp_client.get_request_with_body_and_headers get_update_seen_url body headers) in
+    updated_seen
 end
