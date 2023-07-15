43b0a68b9400f7390e632c625fc1df4ba7594fc8
david-engelmann <david.engelmann44@gmail.com>
2023-07-15 14:53:31 -0400

    add function for notifyOfUpdate

--- src/sync.ml
+++ src/sync.ml
@@ -125,9 +125,9 @@ module Sync = struct
     let application_json = Cohttp_client.application_json_setting_tuple in
     let headers = Cohttp_client.create_headers_from_pairs [application_json; bearer_token] in
     let base_url = App.create_base_url s in
-    let list_repos_url = App.create_endpoint_url base_url (create_sync_endpoint "listRepos") in
-    let body = Cohttp_client.create_body_from_pairs [("limit", string_of_int limit)] in
-    let repos = Lwt_main.run (Cohttp_client.get_request_with_body_and_headers list_repos_url body headers) in
-    repos 
+    let notify_of_update_url = App.create_endpoint_url base_url (create_sync_endpoint "notifyOfUpdate") in
+    let body = Cohttp_client.create_body_from_pairs [("hostname", hostname)] in
+    let notify = Lwt_main.run (Cohttp_client.get_request_with_body_and_headers notify_of_update_url body headers) in
+    notify
 
 end
