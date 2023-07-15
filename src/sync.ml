ea28eda03324f2533a598ca0738d689a62292cbe
david-engelmann <david.engelmann44@gmail.com>
2023-07-15 14:50:11 -0400

    whole bunch of header typos

--- src/sync.ml
+++ src/sync.ml
@@ -93,30 +93,41 @@ module Sync = struct
   let get_repo (s : Session.session) (did : string) (earliest : string) (latest : string) : string =
     let bearer_token = Session.bearer_token_from_session s in
     let application_json = Cohttp_client.application_json_setting_tuple in
-    let headers = Cohttp_client.create_repoers_from_pairs [application_json; bearer_token] in
+    let headers = Cohttp_client.create_headers_from_pairs [application_json; bearer_token] in
     let base_url = App.create_base_url s in
     let get_repo_url = App.create_endpoint_url base_url (create_sync_endpoint "getRepo") in
     let body = Cohttp_client.create_body_from_pairs [("did", did); ("earliest", earliest); ("latest", latest)] in
-    let repo = Lwt_main.run (Cohttp_client.get_request_with_body_and_repoers get_repo_url body headers) in
+    let repo = Lwt_main.run (Cohttp_client.get_request_with_body_and_headers get_repo_url body headers) in
     repo 
 
   let list_blobs (s : Session.session) (did : string) (earliest : string) (latest : string) : string =
     let bearer_token = Session.bearer_token_from_session s in
     let application_json = Cohttp_client.application_json_setting_tuple in
-    let headers = Cohttp_client.create_blobsers_from_pairs [application_json; bearer_token] in
+    let headers = Cohttp_client.create_headers_from_pairs [application_json; bearer_token] in
     let base_url = App.create_base_url s in
     let list_blobs_url = App.create_endpoint_url base_url (create_sync_endpoint "listBlobs") in
     let body = Cohttp_client.create_body_from_pairs [("did", did); ("earliest", earliest); ("latest", latest)] in
-    let blobs = Lwt_main.run (Cohttp_client.get_request_with_body_and_blobsers list_blobs_url body headers) in
+    let blobs = Lwt_main.run (Cohttp_client.get_request_with_body_and_headers list_blobs_url body headers) in
     blobs 
 
   let list_repos (s : Session.session) (limit : int) : string =
     let bearer_token = Session.bearer_token_from_session s in
     let application_json = Cohttp_client.application_json_setting_tuple in
-    let headers = Cohttp_client.create_reposers_from_pairs [application_json; bearer_token] in
+    let headers = Cohttp_client.create_headers_from_pairs [application_json; bearer_token] in
     let base_url = App.create_base_url s in
     let list_repos_url = App.create_endpoint_url base_url (create_sync_endpoint "listRepos") in
     let body = Cohttp_client.create_body_from_pairs [("limit", string_of_int limit)] in
-    let repos = Lwt_main.run (Cohttp_client.get_request_with_body_and_reposers list_repos_url body headers) in
+    let repos = Lwt_main.run (Cohttp_client.get_request_with_body_and_headers list_repos_url body headers) in
     repos 
+
+  let notify_of_update (s : Session.session) (hostname : string) : string =
+    let bearer_token = Session.bearer_token_from_session s in
+    let application_json = Cohttp_client.application_json_setting_tuple in
+    let headers = Cohttp_client.create_headers_from_pairs [application_json; bearer_token] in
+    let base_url = App.create_base_url s in
+    let list_repos_url = App.create_endpoint_url base_url (create_sync_endpoint "listRepos") in
+    let body = Cohttp_client.create_body_from_pairs [("limit", string_of_int limit)] in
+    let repos = Lwt_main.run (Cohttp_client.get_request_with_body_and_headers list_repos_url body headers) in
+    repos 
+
 end
