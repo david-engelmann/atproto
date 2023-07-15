c4439af0a64fea87de31e200e077d98475892675
david-engelmann <david.engelmann44@gmail.com>
2023-07-15 16:13:45 -0400

    add function for muteActor

--- src/bsky/graph.ml
+++ src/bsky/graph.ml
@@ -45,4 +45,15 @@ module Graph = struct
     let body = Cohttp_client.create_body_from_pairs [("limit", string_of_int limit)] in
     let mutes = Lwt_main.run (Cohttp_client.get_request_with_body_and_headers get_mutes_url body headers) in
     mutes
+
+  let mute_actor (s : Session.session) (actor : string) : string =
+    let bearer_token = Session.bearer_token_from_session s in
+    let application_json = Cohttp_client.application_json_setting_tuple in
+    let headers = Cohttp_client.create_headers_from_pairs [application_json; bearer_token] in
+    let base_url = App.create_base_url s in
+    let get_muted_actor_url = App.create_endpoint_url base_url (create_sync_endpoint "muteActor") in
+    let body = Cohttp_client.create_body_from_pairs [("actor", actor)] in
+    let muted_actor = Lwt_main.run (Cohttp_client.get_request_with_body_and_headers get_muted_actor_url body headers) in
+    muted_actor
+    
 end
