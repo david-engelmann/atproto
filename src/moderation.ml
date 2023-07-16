9730b942187e388086bb4e86f8cdc491feae2571
David Engelmann <david+4113@satisfilabs.com>
2023-07-16 12:37:51 -0400

    convert moderation setup to use report_response type

--- src/moderation.ml
+++ src/moderation.ml
@@ -14,8 +14,30 @@ module Moderation = struct
       did : string;
     }
 
+  type report_response =
+    {
+      id : int;
+      created_at : string;
+      reason_type : string;
+      reported_by : string;
+      subject : Yojson.Safe.t;
+    }
+
   let create_moderation_endpoint (query_name : string) : string =
     "com.atproto.moderation" ^ "." ^ query_name
+
+  let convert_body_to_json (body : string) : Yojson.Safe.t =
+    let json = Yojson.Safe.from_string body in
+    json
+
+  let parse_report_response json : report_response =
+    let open Yojson.Safe.Util in
+    let id = json |> member "id" |> to_int in
+    let created_at = json |> member "createdAt" |> to_string in
+    let reason_type = json |> member "reasonType" |> to_string in
+    let reported_by = json |> member "reportedBy" |> to_string in
+    let subject = json |> member "subject" in
+    { id; created_at; reason_type; reported_by; subject; }
 
   let create_subject_from_strong_ref (ref : strong_ref) =
     let subject = `Assoc [
@@ -64,7 +86,7 @@ module Moderation = struct
     in
     Yojson.Basic.to_string report_data
 
-  let create_report_with_strong_ref (s : Session.session) (reason_type : string) ?reason (subject : strong_ref) : string =
+  let create_report_with_strong_ref (s : Session.session) (reason_type : string) ?reason (subject : strong_ref) : report_response =
     let bearer_token = Session.bearer_token_from_session s in
     let application_json = Cohttp_client.application_json_setting_tuple in
     let headers = Cohttp_client.create_headers_from_pairs [application_json; bearer_token] in
@@ -72,9 +94,9 @@ module Moderation = struct
     let create_report_url = App.create_endpoint_url base_url (create_moderation_endpoint "createReport") in
     let data = create_report_data_from_strong_ref reason_type ?reason subject in
     let created_report = Lwt_main.run (Cohttp_client.post_data_with_headers create_report_url data headers) in
-    created_report
+    created_report |> convert_body_to_json |> parse_report_response
 
-  let create_report_with_repo_ref (s : Session.session) (reason_type : string) ?reason (subject : repo_ref) : string =
+  let create_report_with_repo_ref (s : Session.session) (reason_type : string) ?reason (subject : repo_ref) : report_response =
     let bearer_token = Session.bearer_token_from_session s in
     let application_json = Cohttp_client.application_json_setting_tuple in
     let headers = Cohttp_client.create_headers_from_pairs [application_json; bearer_token] in
@@ -82,5 +104,5 @@ module Moderation = struct
     let create_report_url = App.create_endpoint_url base_url (create_moderation_endpoint "createReport") in
     let data = create_report_data_from_repo_ref reason_type ?reason subject in
     let created_report = Lwt_main.run (Cohttp_client.post_data_with_headers create_report_url data headers) in
-    created_report
+    created_report |> convert_body_to_json |> parse_report_response
 end
