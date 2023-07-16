9d1d09e87d7ca236aa8f0439f83cb4c0560c2277
David Engelmann <david+4113@satisfilabs.com>
2023-07-15 23:58:11 -0400

    fix notification calls

--- src/bsky/notification.ml
+++ src/bsky/notification.ml
@@ -6,23 +6,22 @@ module Notification = struct
   let create_notification_endpoint (query_name : string) : string =
     "app.bsky.notification" ^ "." ^ query_name
 
-  let get_unread_count (s : Session.session) (seen_at : string) : string =
+  let get_unread_count (s : Session.session) : string =
     let bearer_token = Session.bearer_token_from_session s in
     let application_json = Cohttp_client.application_json_setting_tuple in
     let headers = Cohttp_client.create_headers_from_pairs [application_json; bearer_token] in
     let base_url = App.create_base_url s in
     let get_unread_count_url = App.create_endpoint_url base_url (create_notification_endpoint "getUnreadCount") in
-    let body = Cohttp_client.create_body_from_pairs [("seenAt", seen_at)] in
-    let unread_count = Lwt_main.run (Cohttp_client.get_request_with_body_and_headers get_unread_count_url body headers) in
+    let unread_count = Lwt_main.run (Cohttp_client.get_request_with_headers get_unread_count_url headers) in
     unread_count
 
-  let list_notifications (s : Session.session) (limit : int) (seen_at : string) : string =
+  let list_notifications (s : Session.session) (limit : int) : string =
     let bearer_token = Session.bearer_token_from_session s in
     let application_json = Cohttp_client.application_json_setting_tuple in
     let headers = Cohttp_client.create_headers_from_pairs [application_json; bearer_token] in
     let base_url = App.create_base_url s in
     let list_notifications_url = App.create_endpoint_url base_url (create_notification_endpoint "listNotifications") in
-    let body = Cohttp_client.create_body_from_pairs [("limit", string_of_int limit); ("seenAt", seen_at)] in
+    let body = Cohttp_client.create_body_from_pairs [("limit", string_of_int limit)] in
     let notifications = Lwt_main.run (Cohttp_client.get_request_with_body_and_headers list_notifications_url body headers) in
     notifications
 
@@ -32,7 +31,7 @@ module Notification = struct
     let headers = Cohttp_client.create_headers_from_pairs [application_json; bearer_token] in
     let base_url = App.create_base_url s in
     let get_update_seen_url = App.create_endpoint_url base_url (create_notification_endpoint "updateSeen") in
-    let body = Cohttp_client.create_body_from_pairs [("seenAt", seen_at)] in
-    let updated_seen = Lwt_main.run (Cohttp_client.get_request_with_body_and_headers get_update_seen_url body headers) in
+    let data = Printf.sprintf "{\"seenAt\": \"%s\"}" seen_at in
+    let updated_seen = Lwt_main.run (Cohttp_client.post_data_with_headers get_update_seen_url data headers) in
     updated_seen
 end
