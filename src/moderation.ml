b1ed3f8cdd40878e4d8386adbd56e0eec745d4f2
David Engelmann <david+4113@satisfilabs.com>
2023-07-16 01:10:47 -0400

    add a function for reporting a repo by did

--- src/moderation.ml
+++ src/moderation.ml
@@ -4,10 +4,15 @@ open Session
 
 module Moderation = struct
   type strong_ref =
-  {
-    uri : string;
-    cid : string;
-  }
+    {
+      uri : string;
+      cid : string;
+    }
+
+  type repo_ref =
+    {
+      did : string;
+    }
 
   let create_moderation_endpoint (query_name : string) : string =
     "com.atproto.moderation" ^ "." ^ query_name
@@ -18,6 +23,16 @@ module Moderation = struct
       ("uri", `String ref.uri);
       ("cid", `String ref.cid);
     ] in
+    Printf.printf "json subject -> %s" (Yojson.Basic.to_string json_subject);
+    let subject = Yojson.Basic.to_string json_subject in
+    subject
+
+  let create_subject_from_repo_ref (ref : repo_ref) : string =
+    let json_subject = `Assoc [
+      ("$type", `String "com.atproto.admin.defs#repoRef");
+      ("did", `String ref.did);
+    ] in
+    Printf.printf "json subject -> %s" (Yojson.Basic.to_string json_subject);
     let subject = Yojson.Basic.to_string json_subject in
     subject
 
@@ -31,6 +46,20 @@ module Moderation = struct
       match reason with
       | Some r -> Printf.sprintf "{\"reasonType\": \"%s\", \"reason\": \"%s\", \"subject\": \"%s\"}" reason_type r (create_subject_from_strong_ref subject)
       | None -> Printf.sprintf "{\"reasonType\": \"%s\", \"subject\": \"%s\"}" reason_type (create_subject_from_strong_ref subject)
+    in
+    let created_report = Lwt_main.run (Cohttp_client.post_data_with_headers create_report_url data headers) in
+    created_report
+
+  let create_report_with_repo_ref (s : Session.session) (reason_type : string) ?reason (subject : repo_ref) : string =
+    let bearer_token = Session.bearer_token_from_session s in
+    let application_json = Cohttp_client.application_json_setting_tuple in
+    let headers = Cohttp_client.create_headers_from_pairs [application_json; bearer_token] in
+    let base_url = App.create_base_url s in
+    let create_report_url = App.create_endpoint_url base_url (create_moderation_endpoint "createReport") in
+    let data =
+      match reason with
+      | Some r -> Printf.sprintf "{\"reasonType\": \"%s\", \"reason\": \"%s\", \"subject\": \"%s\"}" reason_type r (create_subject_from_repo_ref subject)
+      | None -> Printf.sprintf "{\"reasonType\": \"%s\", \"subject\": \"%s\"}" reason_type (create_subject_from_repo_ref subject)
     in
     let created_report = Lwt_main.run (Cohttp_client.post_data_with_headers create_report_url data headers) in
     created_report
