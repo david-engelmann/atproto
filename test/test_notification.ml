8e3a53f620ab3faa57cd4a2eafbb29f935616151
David Engelmann <david+4113@satisfilabs.com>
2023-07-16 19:06:45 -0400

    make sure can walk down to handle for notification test

--- test/test_notification.ml
+++ test/test_notification.ml
@@ -15,9 +15,15 @@ let test_get_unread_count _ =
 
 let test_list_notifications _ =
   let test_session = create_test_session () |> Session.refresh_session_auth in
-  let notification = Notification.list_notifications test_session 10 in
-  Printf.printf "List Notifications: %s\n" notification;
-  OUnit2.assert_bool "List Notifications is not empty" (notification <> "")
+  let notifications = Notification.list_notifications test_session 10 in
+  match notifications with
+  | [] -> OUnit2.assert_equal "blah" ""
+  | hd :: _ ->
+    match hd with
+    | { author; _ } ->
+      match author with
+      | {handle; _ } ->
+        OUnit2.assert_bool "Handle is empty" (String.length handle > 0)
 
 let test_update_seen _ =
   let test_session = create_test_session () |> Session.refresh_session_auth in
