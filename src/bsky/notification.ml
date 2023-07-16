91af7ae4e744571abd7b61ed3b2078d17e6787a0
David Engelmann <david+4113@satisfilabs.com>
2023-07-16 18:59:24 -0400

    return list of notification with list_notifications now

--- src/bsky/notification.ml
+++ src/bsky/notification.ml
@@ -58,6 +58,10 @@ module Notification = struct
   let create_notification_endpoint (query_name : string) : string =
     "app.bsky.notification" ^ "." ^ query_name
 
+  let convert_body_to_json (body : string) : Yojson.Safe.t =
+    let json = Yojson.Safe.from_string body in
+    json
+
   let get_unread_count (s : Session.session) : string =
     let bearer_token = Session.bearer_token_from_session s in
     let application_json = Cohttp_client.application_json_setting_tuple in
@@ -67,7 +71,8 @@ module Notification = struct
     let unread_count = Lwt_main.run (Cohttp_client.get_request_with_headers get_unread_count_url headers) in
     unread_count
 
-  let list_notifications (s : Session.session) (limit : int) : string =
+  let list_notifications (s : Session.session) (limit : int) : notification list =
+    let open Yojson.Safe.Util in
     let bearer_token = Session.bearer_token_from_session s in
     let application_json = Cohttp_client.application_json_setting_tuple in
     let headers = Cohttp_client.create_headers_from_pairs [application_json; bearer_token] in
@@ -75,7 +80,7 @@ module Notification = struct
     let list_notifications_url = App.create_endpoint_url base_url (create_notification_endpoint "listNotifications") in
     let body = Cohttp_client.create_body_from_pairs [("limit", string_of_int limit)] in
     let notifications = Lwt_main.run (Cohttp_client.get_request_with_body_and_headers list_notifications_url body headers) in
-    notifications
+    notifications |> convert_body_to_json |> to_list |> List.map parse_notification
 
   let update_seen (s : Session.session) (seen_at : string) : string =
     let bearer_token = Session.bearer_token_from_session s in
