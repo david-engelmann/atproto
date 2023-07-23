744812c618809b7fe27420bf6416824ae8347e51
David Engelmann <david+4113@satisfilabs.com>
2023-07-23 13:38:58 -0400

    add types and parsing for external embed

--- src/bsky/embed.ml
+++ src/bsky/embed.ml
@@ -18,6 +18,23 @@ module Embed = struct
       size : int;
     }
 
+  type thumb =
+    {
+      thumb_type : string;
+      ref : ref;
+      mime_type : string;
+      size : int;
+
+    }
+
+  type ext =
+    {
+      uri : string;
+      thumb : thumb;
+      title : string;
+      description : string;
+    }
+
   type image_data =
     {
       alt : string;
@@ -30,9 +47,16 @@ module Embed = struct
       images : image_data list;
     }
 
+  type ext_embed =
+    {
+      embed_type : string;
+      ext : ext;
+    }
+
   type embed = (* GONNA CHANGE TO | style *)
     [
     | `Image of image_embed
+    | `External of ext_embed
     ]
 
   let parse_ref json : ref =
@@ -47,6 +71,28 @@ module Embed = struct
     let mime_type = json |> member "mimeType" |> to_string in
     let size = json |> member "size" |> to_int in
     { image_type; ref; mime_type; size }
+
+  let parse_thumb json : thumb =
+    let open Yojson.Safe.Util in
+    let thumb_type = json |> member "$type" |> to_string in
+    let ref = json |> member "ref" |> parse_ref in
+    let mime_type = json |> member "mimeType" |> to_string in
+    let size = json |> member "size" |> to_int in
+    { thumb_type; ref; mime_type; size }
+
+  let parse_ext json : ext =
+    let open Yojson.Safe.Util in
+    let uri = json |> member "uri" |> to_string in
+    let thumb = json |> member "thumb" |> parse_thumb in
+    let title = json |> member "title" |> to_string in
+    let description = json |> member "description" |> to_string in
+    { uri; thumb; title; description }
+
+  let parse_ext_embed json : ext_embed =
+    let open Yojson.Safe.Util in
+    let embed_type = json |> member "$type" |> to_string in
+    let ext = json |> member "external" |> parse_ext in
+    { embed_type; ext }
 
   let parse_image_data json : image_data =
     let open Yojson.Safe.Util in
